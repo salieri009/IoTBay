5cefa2056c43ea80a57d4d38a04e9ed80e3c81f2
Salieri009 <Jungwook.Van-1@student.uts.edu.au>
2025-05-25 12:26:21 +1000

    fix

--- src/main/java/controller/ManageAccessLogController.java
+++ src/main/java/controller/ManageAccessLogController.java
@@ -7,7 +7,10 @@ import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.AccessLogDAOImpl;
 import dao.interfaces.AccessLogDAO;
